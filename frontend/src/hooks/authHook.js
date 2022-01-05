b14ad4659df09e1fe26f30d7fa75c0a27b88a003
FrancoRodao <francorodao802@gmail.com>
2022-01-05 02:24:57 -0300

    frontend: signin bug solved

--- frontend/src/hooks/authHook.js
+++ frontend/src/hooks/authHook.js
@@ -24,6 +24,7 @@ export function useSignIn(
       delete userData.carrito
       delete userData.role
       delete userData.provider
+      delete userData.pedidos
 
       dispatch({
         type: types.signIn,
