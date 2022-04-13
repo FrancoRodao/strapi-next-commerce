970a50fe04207ccec9f83b3064228bba00781a69
FrancoRodao <francorodao802@gmail.com>
2022-04-13 02:22:28 -0300

    frontend: responsive footer

--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -1,64 +1,65 @@
 import styled from 'styled-components'
 
-const Container = styled.div`
+const MainContainer = styled.div`
   background-color: ${({ theme }) => theme.navbarBgColor};
+  text-align: center;
+  padding: 15px;
+`
+
+const Container = styled.div`
   display: flex;
-  justify-content: space-around;
-  padding: 5px;
-  margin-top: 30px;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  ul {
+    margin: 15px;
+  }
+`
+
+const Paragraph = styled.p`
+  margin-top: 15px;
 `
 
 export default function Footer() {
   return (
-    <Container>
-      <ul>
-        <li>
-          King Mobile
-          <ul>
-            <li>
-              <a href="">kingmobilelegends@gmail.com</a>
-            </li>
-            <li>
-              <a href="">26133445</a>
-            </li>
-            <li>
-              <a href="">Rodeo del sur 5043</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          King Mobile
-          <ul>
-            <li>
-              <a href="">kingmobilelegends@gmail.com</a>
-            </li>
-            <li>
-              <a href="">26133445</a>
-            </li>
-            <li>
-              <a href="">Rodeo del sur 5043</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          King Mobile
-          <ul>
-            <li>
-              <a href="">kingmobilelegends@gmail.com</a>
-            </li>
-            <li>
-              <a href="">26133445</a>
-            </li>
-            <li>
-              <a href="">Rodeo del sur 5043</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </Container>
+    <MainContainer>
+      King Mobile
+      <Container>
+        <ul>
+          <li>
+            <Paragraph>kingmobilelegends@gmail.com</Paragraph>
+          </li>
+          <li>
+            <Paragraph>26133445</Paragraph>
+          </li>
+          <li>
+            <Paragraph>Rodeo del sur 5043</Paragraph>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Paragraph>kingmobilelegends@gmail.com</Paragraph>
+          </li>
+          <li>
+            <Paragraph>26133445</Paragraph>
+          </li>
+          <li>
+            <Paragraph>Rodeo del sur 5043</Paragraph>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Paragraph>kingmobilelegends@gmail.com</Paragraph>
+          </li>
+          <li>
+            <Paragraph>26133445</Paragraph>
+          </li>
+          <li>
+            <Paragraph>Rodeo del sur 5043</Paragraph>
+          </li>
+        </ul>
+      </Container>
+    </MainContainer>
   )
 }
